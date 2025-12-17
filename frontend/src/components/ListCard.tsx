13656d2c7db040da532619cb909fbc25b57780b7
Abiral Man Rajbhandari <153396173+abiralrajbhandari@users.noreply.github.com>
2025-12-17 21:18:12 +0545

    feat: Add logic to complete, delete, and edit list items

--- frontend/src/components/ListCard.tsx
+++ frontend/src/components/ListCard.tsx
@@ -3,19 +3,23 @@ import type { ListItemsType } from "./ListItem";
 import { Ellipsis, SquarePen, Trash } from "lucide-react";
 
 interface ListCardProps {
-  items: ListItemsType;
+  item: ListItemsType;
+  index: number;
+  handleComplete: (index: number) => void; // function type
+  handleEdit: (index: number) => void;
+  handleDelete: (index: number) => void;
 }
 
-function ListCard({ items }: ListCardProps) {
+function ListCard({ item, index, handleComplete, handleEdit, handleDelete }: ListCardProps) {
   const [dropDown, setDropDown] = useState(false);
   return (
     <div className="flex justify-between bg-white border border-gray-200 rounded-lg hover:shadow-md">
       {/* div1: Checkbox + List Details */}
       <div className="flex items-center p-5 gap-5 ">
-        <input className="h-4 w-4 cursor-pointer" type="checkbox" />
-        <div className="space-y-2">
-          <h2 className="text-lg  font-bold">{items.itemName} </h2>
-          <p className="text-sm text-gray-500">Qty: {items.quantity} </p>
+        <input className="h-4 w-4 cursor-pointer" type="checkbox" checked = {item.completed} onChange={() => handleComplete(index)} />
+        <div className= {`space-y-2 ${item.completed ? "line-through text-gray-500" : "none"}`} >
+          <h2 className="text-lg  font-bold">{item.itemName} </h2>
+          <p className="text-sm text-gray-500">Qty: {item.quantity} </p>
         </div>
       </div>
       {/* div2: Edit/Delete List */}
@@ -25,17 +29,17 @@ function ListCard({ items }: ListCardProps) {
           onClick={() => setDropDown(!dropDown)}>
           {dropDown && (
             <ul className="absolute right-0 top-10 w-30 bg-white border border-gray-200 rounded-md shadow-lg z-10">
-              <li className="flex items-center gap-2 px-4 py-2 hover:bg-gray-100 transition-colors cursor-pointer">
+              <li onClick={() => handleEdit(index)} className="flex items-center gap-2 px-4 py-2 hover:bg-gray-100 transition-colors cursor-pointer">
                 <SquarePen className="h-4 w-4" /> Edit
               </li>
-              <li className="flex items-center gap-2 px-4 py-2 hover:bg-gray-100 transition-colors cursor-pointer">
+              <li onClick={() =>handleDelete(index)} className="flex items-center gap-2 px-4 py-2 hover:bg-gray-100 transition-colors cursor-pointer">
                 <Trash className="h-4 w-4" /> Delete
               </li>
             </ul>
           )}
           <Ellipsis />
         </button>
-        <p className="text-gray-800 font-medium">Rs {items.price}/-</p>
+        <p className="text-gray-800 font-medium">Rs {item.price}/-</p>
       </div>
     </div>
   );
